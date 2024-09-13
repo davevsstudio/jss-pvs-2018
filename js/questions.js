05233c1cbb8d816f561cfec8c0c73c2b26fd2a2e
davestar <142616037+davevsstudio@users.noreply.github.com>
2024-09-12 22:24:49 -0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,301 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of this NOT an agricultural cooperative society?",
+    answer: "consumer cooperatives",
+    options: [
+      "consumer cooperatives",
+      "producer copperatives",
+      "multipurpose cooperatives",
+      "finance"
+    ]
+  },
+    {
+    numb: 2,
+    question: "When cocoa is transported from Nigeria to America for trade. Nigeria is ______ country",
+    answer: "export",
+    options: [
+      "import",
+      "transport",
+      "export",
+      "passport"
+    ]
+  },
+    {
+    numb: 3,
+    question: "One of these crops is not grown in Nigeria  for export",
+    answer: "rice",
+    options: [
+      "cashew",
+      "rice",
+      "rubber",
+      "groundnut"
+    ]
+  },
+    {
+    numb: 4,
+    question: "In stock market, a person or organization that holds some shares is called?",
+    answer: "shareholder",
+    options: [
+      "shareholder",
+      "stockholder",
+      "shop holder",
+      "chair holder"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The physical method of controlling crop-pest is killing pest by/with",
+    answer: "handpicking",
+    options: [
+      "handpicking",
+      "use of their enemies",
+      "destorying the environment of the pest",
+      "chemical"
+    ]
+  },
+  {
+    numb: 6,
+    question: "_______ is the total destruction of forest by human or other activities",
+    answer: "deforestation",
+    options: [
+      "afforestation",
+      "deforestation",
+      "tanugy system",
+      "forestation"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "The sum total of food required by animal for a day is",
+    answer: "ration",
+    options: [
+      "nutrient",
+      "fodder",
+      "ration",
+      "diet"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Which of these disease is NOG caused by virus",
+    answer: "mastitis",
+    options: [
+      "food and mouth",
+      "cattle pque",
+      "newcastle",
+      "mastitis"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Aquatic animals that lives in pons, lakes, rivers and streams and are regarded as ________ water animals",
+    answer: "fresh water",
+    options: [
+      "ocean",
+      "marine",
+      "salt",
+      "fresh water"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "The use of machinery, skilled labour, large areas of land, high yields of crops and modern storage facilities are the characteristics of _________ plant",
+    answer: "commercial",
+    options: [
+      "subsistence",
+      "mixed",
+      "commercial",
+      "plantation"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "Which of this this animal serve as means of transportation",
+    answer: "donkey",
+    options: [
+      "pig",
+      "goat",
+      "donkey",
+      "sheep"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "The following are farm structure EXCEPT",
+    answer: "weighbridge",
+    options: [
+      "silo",
+      "crib",
+      "rhumbus",
+      "weighbridge"
+    ]
+  },
+
+
+  {
+    numb: 13,
+    question: "The reproduction part of plant is",
+    answer: "flower",
+    options: [
+      "leaf",
+      "seed",
+      "stem",
+      "flower"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "It has parallel vein leaf, fibrous root, these are the characterictics of ________ plant",
+    answer: "monocotyledon",
+    options: [
+      "monocotyledon",
+      "dicotyledon",
+      "cocoa",
+      "orange"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "Crocodiles, alligators and turtles are example of aqutic",
+    answer: "reptiles",
+    options: [
+      "reptiles",
+      "mammals",
+      "amphibians",
+      "arthropod"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "A conical or cylindral  plastic sheet used to hold water in poultry is called",
+    answer: "water through",
+    options: [
+      "feeding through",
+      "water through",
+      "container",
+      "poultry through"
+    ]
+  },
+
+
+  {
+    numb: 17,
+    question: "The removal of surplus seeding from areas sown with seed is _______",
+    answer: "thinning",
+    options: [
+      "mulching",
+      "supplying",
+      "thinning",
+      "harvesting"
+    ]
+  },
+
+  {
+    numb: 18,
+    question: "All these are agricultural product EXCEPT",
+    answer: "plastic",
+    options: [
+      "kenaf",
+      "cotten",
+      "rubber",
+      "plastic"
+    ]
+  },
+
+  {
+    numb: 19,
+    question: "Trypanosome is transmitted by a vector",
+    answer: "tsetse fly",
+    options: [
+      "tsetse fly",
+      "white fly",
+      "house fly",
+      "aphids"
+    ]
+  },
+
+
+
+  {
+    numb: 20,
+    question: "Which of the following is energy feed concentrate",
+    answer: "maize",
+    options: [
+      "pigeon pea",
+      "maize",
+      "groundnut cake",
+      "soya bean meal"
+    ]
+  },
+
+
+  {
+    numb: 21,
+    question: "Stalking is a pratice which is beneficial to one of the following crops",
+    answer: "yam and pumpkin",
+    options: [
+      "yam and cocoyam",
+      "rice and wheat",
+      "yam and pumpkin",
+      "tobacco and tomato"
+    ]
+  },
+
+  {
+    numb: 22,
+    question: "The following are aquatic animals except",
+    answer: "crocodile and snakes",
+    options: [
+      "fish and prawns",
+      "lobsters and crayfish",
+      "oysters and periwinkles",
+      "crocodile and snakes"
+    ]
+  },
+
+  {
+    numb: 23,
+    question: "When product are in direct contact with packaging, it is a _______ packaging  ",
+    answer: "primary",
+    options: [
+      "primary",
+      "secondary",
+      "tertiary",
+      "universal"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: "Causative organism of anthrax disease is",
+    answer: "bacteria",
+    options: [
+      "fungi",
+      "bacteria",
+      "virus",
+      "protozoa"
+    ]
+  },
+
+  {
+    numb: 25,
+    question: "The priice of agricultural product will be affected by the following EXCEPT",
+    answer: "advertisement of product",
+    options: [
+      "transportation cost",
+      "seasonality cost",
+      "advertisement of product",
+      "perish ability of the product"
+    ]
+  },
+];
